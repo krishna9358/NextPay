628b912efe8e778ab714e3eee8dfa99542376c4b
Krishna <krishanmohank974@gmail.com>
2024-08-05 12:15:22 +0530

    added appBar

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import {PrismaClient} from "@repo/db/client";
+import { Appbar } from "../../../packages/ui/src/AppBar";
 // import { useBalance } from "@repo/store/useBalance";
-
+import { signIn, signOut, useSession } from "next-auth/react";
 const client = new PrismaClient();
 
 export default function() {
+  const session = useSession();
   // const balance = useBalance();
   return <div>
-    hi there 
+    <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}></Appbar>
   </div>
 }
