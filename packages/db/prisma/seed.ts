9eab025538221c2e6c4f1c4f8b985f16d9b474e3
Krishna <krishanmohank974@gmail.com>
2024-08-10 16:36:27 +0530

    added db-seed-file

---
+++ packages/db/prisma/seed.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { number: '9999999999', email:"example@gmail.com" },
+    update: {},
+    create: {
+        email: "example@gmail.com",
+      number: '9999999999',
+      password: 'alice',
+      name: 'alice',
+      OnRampTransaction: {
+        create: {
+          startTime: new Date(),
+          status: "Success",
+          amount: 20000,
+          token: "122",
+          provider: "HDFC Bank",
+        },
+      },
+    },
+  })
+  const bob = await prisma.user.upsert({
+    where: { number: '9999999998', email:"examle2@gmail.com" },
+    update: {},
+    create: {
+      number: '9999999998',
+      email:"example2@gmail.com",
+      password: 'bob',
+      name: 'bob',
+      OnRampTransaction: {
+        create: {
+          startTime: new Date(),
+          status: "Failure",
+          amount: 2000,
+          token: "123",
+          provider: "HDFC Bank",
+        },
+      },
+    },
+  })
+  console.log({ alice, bob })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    // process.exit(1)
+  })
