2e70dbe4e99d5fe647af817b80243cdf257ec737
Krishna <krishanmohank974@gmail.com>
2024-08-10 14:28:54 +0530

    added transaction in amount

--- apps/bank_webhook/src/index.ts
+++ apps/bank_webhook/src/index.ts
@@ -2,15 +2,22 @@ import express from "express";
 import db from "@repo/db/client"
 const app = express();
 
+interface paymentInformation {
+    token : string,
+    userId : number,
+    amount : number
+}
 
 app.post("/hdfcWebhook", async (req, res)=>{
-    const paymentInformation = {
+    const paymentInformation : paymentInformation= {
         token : req.body.token,
         userId : req.body.userId,
-        amount : req.body.amount
+        amount : req.body.amount,
     }
-    // transaction 
-    await db.balance.update({
+    // transaction
+    try {
+        await db.$transaction([
+     db.balance.update({
         where:{
             userId : paymentInformation.userId,
         },
@@ -19,9 +26,8 @@ app.post("/hdfcWebhook", async (req, res)=>{
                 increment : paymentInformation.amount,
             }
         }
-    })
-
-    await db.onRampTransaction.update({
+    }),
+     db.onRampTransaction.update({
         where:{
             token: paymentInformation.token,
         },
@@ -29,11 +35,16 @@ app.post("/hdfcWebhook", async (req, res)=>{
             status: "Success"
         }
     })
-
+]);
     res.status(200).json({
         message: "caputred"
     })
-
+}catch(e){
+    console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+});
+}
 })
 
 
