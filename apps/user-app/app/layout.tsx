1aae86727ab2d1f2adf92f4160d50c412c76a3d7
Krishna <krishanmohank974@gmail.com>
2024-08-10 15:38:03 +0530

    added frontend for dashboard and transfer

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../providers";
+import { AppbarClient } from "../AppBarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>{children}</body>
+        
+        <body className={inter.className}>
+        <AppbarClient></AppbarClient>
+          {children}
+          
+          </body>
       </Providers>
     </html>
   );
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { Appbar } from "../../../packages/ui/src/AppBar";
+import { Appbar } from "@repo/ui/AppBar";
 // import { useBalance } from "@repo/store/useBalance";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { AppbarClient } from "../AppBarClient";
 
 export default function() {
   const session = useSession();
   // const balance = useBalance();
   return <div>
-    <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}></Appbar>
+    {/* <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}></Appbar> */}
+    {/* <AppbarClient></AppbarClient> */}
   </div>
 }
